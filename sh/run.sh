8d3a39730d9e7366851f81cb051583f3b93d9563
fabiobatalha <fabiobatalha@gmail.com>
2015-07-31 16:49:34 -0300

    Geração de csv de seções

--- sh/run.sh
+++ sh/run.sh
@@ -17,24 +17,19 @@ colls=`ls -1 $processing_path/iso`
 rm -f $processing_path/databases/isis/artigo.*
 rm -f $processing_path/databases/isis/title.*
 rm -f $processing_path/databases/isis/bib4cit.*
+rm -f $processing_path/databases/isis/issue.*
 
 for coll in $colls;
 do
    echo "Creating now "$coll" master files"
-   if [[ $mode == "scp" ]]; then
-       scp $processing_user@$processing_server:/bases/org.000/iso/art.$coll/artigo.iso $processing_path/iso/$coll/artigo.iso
-       scp $processing_user@$processing_server:/bases/org.000/iso/tit.$coll/title.iso $processing_path/iso/$coll/title.iso
-       scp $processing_user@$processing_server:/bases/org.000/iso/b4c.$coll/bib4cit.iso $processing_path/iso/$coll/bib4cit.iso
-   fi
    $cisis_dir/mx iso=$processing_path/iso/$coll/artigo.iso "proc='a992#$coll#'" append=$processing_path/databases/isis/artigo -all now
    $cisis_dir/mx iso=$processing_path/iso/$coll/title.iso "proc='a992#$coll#'" append=$processing_path/databases/isis/title -all now
    $cisis_dir/mx iso=$processing_path/iso/$coll/bib4cit.iso "proc='a992#$coll#'" append=$processing_path/databases/isis/bib4cit -all now
-   if [[ $mode == "scp" ]]; then
-       rm $processing_path/iso/$coll/artigo.iso
-       rm $processing_path/iso/$coll/title.iso
-       rm $processing_path/iso/$coll/bib4cit.iso
-   fi
+   $cisis_dir/mx iso=$processing_path/iso/$coll/issue.iso "proc='a992#$coll#'" append=$processing_path/databases/isis/issue -all now
 done
+
+echo "Creating Section CSV"
+$cisis_dir/mx $processing_path/databases/isis/issue "pft=if p(v49) then ('\"',v992[1],'\",\"',v49^l,'\",\"',v49^c,'\",\"',v49^t,'\"'/) fi" -all now > $processing_path/output/section_codes.csv
 
 echo "Indexing databases according to FSTs"
 $cisis_dir/mx $processing_path/databases/isis/artigo  fst="@$processing_path/fst/artigo.fst"  fullinv/ansi=$processing_path/databases/isis/artigo  tell=1000  -all now
