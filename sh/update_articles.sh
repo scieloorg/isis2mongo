203712533b431899c574ad56ca5f1bd01773db61
fabiobatalha <fabiobatalha@gmail.com>
2015-10-16 10:31:25 -0300

    ajustes em update_articles

--- sh/update_articles.sh
+++ sh/update_articles.sh
@@ -24,27 +24,22 @@ do
     echo $from"/"$total_pids "-" $pid
     from=$(($from+1))
 
-    loaded=`curl -s -X GET "http://"$scielo_data_url"/api/v1/article/exists/?code=$pid&collection=$collection"`
-    if [[ $loaded == "true" ]]; then
-        mkdir -p $processing_path/output/isos/$pid
-        issn=${pid:1:9}
-        len=${#pid}
-        if [[ $len -eq 23 ]]; then
-            $cisis_dir/mx $processing_path/databases/isis/artigo  btell="0" $collection$pid count=1 iso=$processing_path/output/isos/$pid/$pid"_artigo.iso" -all now
-            $cisis_dir/mx $processing_path/databases/isis/title   btell="0" $collection$issn count=1 iso=$processing_path/output/isos/$pid/$pid"_title.iso" -all now
-            $cisis_dir/mx $processing_path/databases/isis/bib4cit btell="0" $collection$pid"$" iso=$processing_path/output/isos/$pid/$pid"_bib4cit.iso" -all now
-            cd sh
-            ./isis2json.py $processing_path/output/isos/$pid/$pid"_artigo.iso" -c -p v -t 3 > $processing_path/output/isos/$pid/$pid"_artigo.json"
-            ./isis2json.py $processing_path/output/isos/$pid/$pid"_title.iso" -c -p v -t 3 > $processing_path/output/isos/$pid/$pid"_title.json"
-            ./isis2json.py $processing_path/output/isos/$pid/$pid"_bib4cit.iso" -c -p v -t 3 > $processing_path/output/isos/$pid/$pid"_bib4cit.json"
-            ./packing_json.py 'article' $pid > $processing_path/output/isos/$pid/$pid"_package.json"
-            cd ..
-            curl -H "Content-Type: application/json" --data @$processing_path/output/isos/$pid/$pid"_package.json" -X POST "http://"$scielo_data_url"/api/v1/article/update/?admintoken="$admintoken
-            #rm -rf $processing_path/output/isos/$pid
-        fi
-    else
-        echo "article alread processed!!!"
+    mkdir -p $processing_path/output/isos/$pid
+    issn=${pid:1:9}
+    len=${#pid}
+    if [[ $len -eq 23 ]]; then
+        $cisis_dir/mx $processing_path/databases/isis/artigo  btell="0" $collection$pid count=1 iso=$processing_path/output/isos/$pid/$pid"_artigo.iso" -all now
+        $cisis_dir/mx $processing_path/databases/isis/title   btell="0" $collection$issn count=1 iso=$processing_path/output/isos/$pid/$pid"_title.iso" -all now
+        $cisis_dir/mx $processing_path/databases/isis/bib4cit btell="0" $collection$pid"$" iso=$processing_path/output/isos/$pid/$pid"_bib4cit.iso" -all now
+        cd sh
+        ./isis2json.py $processing_path/output/isos/$pid/$pid"_artigo.iso" -c -p v -t 3 > $processing_path/output/isos/$pid/$pid"_artigo.json"
+        ./isis2json.py $processing_path/output/isos/$pid/$pid"_title.iso" -c -p v -t 3 > $processing_path/output/isos/$pid/$pid"_title.json"
+        ./isis2json.py $processing_path/output/isos/$pid/$pid"_bib4cit.iso" -c -p v -t 3 > $processing_path/output/isos/$pid/$pid"_bib4cit.json"
+        ./packing_json.py 'article' $pid > $processing_path/output/isos/$pid/$pid"_package.json"
+        cd ..
+        curl -H "Content-Type: application/json" --data @$processing_path/output/isos/$pid/$pid"_package.json" -X POST "http://"$scielo_data_url"/api/v1/article/update/?admintoken="$admintoken
+        rm -rf $processing_path/output/isos/$pid
     fi
 done
 
-> $processing_path/sh/update_identifiers.txt
+echo '' > $processing_path/sh/update_identifiers.txt
