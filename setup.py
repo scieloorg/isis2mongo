0d4df822089a8c9af4b408a01f5320d7e3072a8a
fabiobatalha <fabiobatalha@gmail.com>
2017-07-12 11:42:32 -0300

    Ajuste para data de processamento

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requires = []
 
 setup(
     name="isis2mongo",
-    version='1.12.0',
+    version='1.12.1',
     description="Processamento de alimentação do articlemeta",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
