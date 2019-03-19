09848d795d07b44a92ce5daede93699d95289d67
Joffily Ferreira <joffily@outlook.com>
2019-03-19 16:04:09 -0300

    WIP

--- isis2mongo/controller.py
+++ isis2mongo/controller.py
@@ -263,3 +263,98 @@ class DataBroker(object):
             logger.debug('Recorded (%s)', record['collection']+record['code'])
         except:
             logger.exception('Fail to write record (%s)', record['collection']+record['code'])
+
+
+class JsonBroker:
+
+    def __init__(self, data={}):
+        self._data = data
+
+    def __exit__(self):
+        pass
+
+    def __enter__(self):
+        pass
+
+    def bulk_data(self, bulk):
+        for collection, records in bulk.items():
+                self._data[collection] = records
+                print("Adicionando %s" % collection)
+
+    @property
+    def journals_ids(self):
+        journals = []
+        for journal in self._data['journals']:
+            journals.append(
+              '_'.join(
+                [journal['collection'], journal['code'], journal['processing_date'].replace('-', '')])
+            )
+
+        return journals
+
+
+    @property
+    def issues_ids(self):
+        issues = []
+        for issue in self._data['issues']:
+            issues.append(
+              '_'.join(
+                [issue['collection'], issue['code'], issue['processing_date'].replace('-', '')])
+            )
+
+        return issues
+
+    @property
+    def articles_ids(self):
+        articles = []
+        for article in self._data['articles']:
+            articles.append(
+              '_'.join(
+                [article['collection'], article['code'], article['processing_date'].replace('-', '')])
+            )
+        return articles
+
+    @property
+    def references_ids(self):
+        references = []
+        for reference in self._data['references']:
+            references.append(
+              '_'.join(
+                [reference['collection'], reference['code'], reference['processing_date'].replace('-', '')])
+            )
+
+        return references
+
+
+    def load_journal(self, collection, pid):
+        for journal in self._data['journals']:
+            if journal["code"] == pid and \
+                journal["collection"] == collection:
+                return journal
+
+    def load_issue(self, collection, pid):
+
+        metadata = {}
+        issue_metadata = None
+
+        for issue in self._data['issues']:
+            if issue["code"] == pid and \
+                issue["collection"] == collection:
+                issue_metadata = issue
+                break # too ugly
+
+        journal_metadata = self.load_journal(collection, pid[:9])
+
+        try:
+            del(journal_metadata['_id'])
+            del(issue_metadata['_id'])
+            del(journal_metadata['journal'])
+            del(issue_metadata['journal'])
+            del(issue_metadata['issue'])
+        except KeyError:
+            pass
+
+        metadata['title'] = journal_metadata
+        metadata['issue'] = issue_metadata
+
+        return metadata
--- isis2mongo/isis2mongo.py
+++ isis2mongo/isis2mongo.py
@@ -14,7 +14,7 @@ from articlemeta.client import ThriftClient
 from articlemeta.client import UnauthorizedAccess
 from articlemeta.client import ServerError
 
-from controller import DataBroker, IsisDataBroker
+from controller import DataBroker, IsisDataBroker, JsonBroker
 
 logger = logging.getLogger(__name__)
 
@@ -22,8 +22,8 @@ logger = logging.getLogger(__name__)
 DATABASES = (
     ('title', 'journals'),
     ('issue', 'issues'),
-    ('artigo', 'articles'),
-    ('bib4cit', 'references'),
+    # ('artigo', 'articles'),
+    # ('bib4cit', 'references'),
 )
 BULK_SIZE = 100000
 SECURE_ARTICLE_DELETIONS_NUMBER = 2000
@@ -31,7 +31,9 @@ SECURE_ISSUE_DELETIONS_NUMBER = 20
 SECURE_JOURNAL_DELETIONS_NUMBER = 5
 ADMINTOKEN = os.environ.get('ARTICLEMETA_ADMINTOKEN', 'admin')
 ARTICLEMETA_THRIFTSERVER = os.environ.get('ARTICLEMETA_THRIFTSERVER', 'admin')
-ISO_PATH = os.environ.get('ISO_PATH', os.path.dirname(os.path.abspath(__file__)))
+# ISO_PATH = os.environ.get('ISO_PATH', os.path.dirname(os.path.abspath(__file__)))
+ISO_PATH = "/Users/joffilyferreira/Downloads/"
+
 SENTRY_HANDLER = os.environ.get('SENTRY_HANDLER', None)
 LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', None)
 LOGGING = {
@@ -149,8 +151,7 @@ def load_isis_records(collection, issns=None):
     for iso, coll in DATABASES:
         rec_coll = coll
         logger.info('Recording (%s) records for collection (%s)', coll, collection)
-        isofile = '%s/../isos/%s/%s.iso' % (ISO_PATH, collection, iso)
-
+        isofile = '%s/isos/%s/%s.iso' % (ISO_PATH, collection, iso)
         try:
             isis_db = IsisDataBroker(isofile)
         except IOError:
@@ -268,310 +269,138 @@ def load_articlemeta_journals_ids(collection, issns=None):
     return journals_pids
 
 
-def run(collection, issns, full_rebuild=False, force_delete=False, bulk_size=BULK_SIZE):
+def run(collection, issns, full_rebuild=True, force_delete=False, bulk_size=BULK_SIZE):
 
-    rc = ThriftClient(domain=ARTICLEMETA_THRIFTSERVER, admintoken=ADMINTOKEN)
+    # rc = ThriftClient(domain=ARTICLEMETA_THRIFTSERVER, admintoken=ADMINTOKEN)
+    rc = {}
 
     logger.info('Running Isis2mongo')
     logger.debug('Thrift Server: %s', ARTICLEMETA_THRIFTSERVER)
     logger.debug('Admin Token: %s', ADMINTOKEN)
     logger.info('Loading ArticleMeta identifiers for collection: %s', collection)
 
-    articlemeta_documents = set(
-        load_articlemeta_documents_ids(collection, issns))
-    articlemeta_issues = set(
-        load_articlemeta_issues_ids(collection, issns))
-    articlemeta_journals = set(
-        load_articlemeta_journals_ids(collection, issns))
+    articlemeta_documents = set([])
+    articlemeta_issues = set([])
+    articlemeta_journals = set([])
 
-    if full_rebuild is True:
-        articlemeta_documents = set([])
-        articlemeta_issues = set([])
-        articlemeta_journals = set([])
 
-    with DataBroker(uuid.uuid4()) as ctrl:
-        update_issue_id = ''
+    ctrl = JsonBroker(data={"journals": [], "issues": [], "articles": []})
+    update_issue_id = ''
+    fields_to_update_after_loading_documents = []
+    bulk = {}
 
-        fields_to_update_after_loading_documents = []
-        bulk = {}
+    bulk_count = 0
+    for coll, record in load_isis_records(collection, issns):
+        bulk_count += 1
+        bulk.setdefault(coll, [])
+        bulk[coll].append(record)
+        if bulk_count == bulk_size:
+            bulk_count = 0
+            ctrl.bulk_data(dict(bulk))
+            bulk = {}
 
-        bulk_count = 0
-        for coll, record in load_isis_records(collection, issns):
-            bulk_count += 1
-            bulk.setdefault(coll, [])
-            bulk[coll].append(record)
-            if bulk_count == bulk_size:
-                bulk_count = 0
-                ctrl.bulk_data(dict(bulk))
-                bulk = {}
-
-            # ctrl.write_record(coll, record)
-            # Write field 4 in issue database
-            rec_type = record.get('v706', [{'_': ''}])[0]['_']
-            if rec_type == 'h':
-                if update_issue_id == record['v880'][0]['_'][1:18]:
-                    continue
-                fields_to_update_after_loading_documents.append([
-                    'issues',
-                    record['v880'][0]['_'][1:18],
-                    'v4',
-                    record['v4'][0]['_']
-                ])
-        # bulk residual data
-        ctrl.bulk_data(dict(bulk))
-
-        logger.info('Updating fields metadata')
-        ctrl.bulk_update_field('issues', fields_to_update_after_loading_documents)
-        logger.info('Loading legacy identifiers')
-        legacy_documents = set(ctrl.articles_ids)
-        legacy_issues = set(ctrl.issues_ids)
-        legacy_journals = set(ctrl.journals_ids)
-
-        logger.info('Producing lists of differences between ArticleMeta and Legacy databases')
-        new_documents = list(legacy_documents - articlemeta_documents)
-        new_issues = list(legacy_issues - articlemeta_issues)
-        new_journals = list(legacy_journals - articlemeta_journals)
-
-        am_document_pids_only = set([i[0:27] for i in articlemeta_documents])
-        lg_document_pids_only = set([i[0:27] for i in legacy_documents])
-        to_remove_documents = list(am_document_pids_only - lg_document_pids_only)
-
-        am_issue_pids_only = set([i[0:21] for i in articlemeta_issues])
-        lg_issue_pids_only = set([i[0:21] for i in legacy_issues])
-        to_remove_issues = list(am_issue_pids_only - lg_issue_pids_only)
-
-        am_journals_pids_only = set([i[0:13] for i in articlemeta_journals])
-        lg_journals_pids_only = set([i[0:13] for i in legacy_journals])
-        to_remove_journals = list(am_journals_pids_only - lg_journals_pids_only)
-
-        # Removing Documents
-        total_to_remove_documents = len(to_remove_documents)
-        logger.info(
-            'Documents to be removed from articlemeta (%d)',
-            total_to_remove_documents
-        )
-
-        skip_deletion = True
-        if total_to_remove_documents > SECURE_ARTICLE_DELETIONS_NUMBER:
-            logger.info('To many documents to be removed')
-            if force_delete is False:
-                skip_deletion = True
-                logger.info('force_delete is setup to %s, the remove task will be skipped', force_delete)
-            else:
-                skip_deletion = False
-
-        for ndx, item in enumerate(to_remove_documents, 1):
-            item = item.split('_')
-            if skip_deletion is True:
-                logger.debug(
-                    'Document remove task (%d, %d) will be skipped (%s)',
-                    ndx,
-                    total_to_remove_documents,
-                    '_'.join([item[0], item[1]])
-                )
-            try:
-                rc.delete_document(item[1], item[0])
-                logger.debug(
-                    'Document (%d, %d) removed from Articlemeta (%s)',
-                    ndx,
-                    total_to_remove_documents,
-                    '_'.join([item[0], item[1]])
-                )
-            except UnauthorizedAccess:
-                logger.warning('Unauthorized access to remove itens, check the ArticleMeta admin token')
-
-        # Including and Updating Documents
-        logger.info(
-            'Documents being included into articlemeta (%d)',
-            len(new_documents)
-        )
-        for ndx, item in enumerate(new_documents, 1):
-            item = item.split('_')
-            try:
-                document_meta = ctrl.load_document(item[0], item[1])
-            except:
-                logger.error(
-                    'Fail to load document into Articlemeta (%s)',
-                    '_'.join([item[0], item[1]])
-                )
+        # ctrl.write_record(coll, record)
+        # Write field 4 in issue database
+        rec_type = record.get('v706', [{'_': ''}])[0]['_']
+        if rec_type == 'h':
+            if update_issue_id == record['v880'][0]['_'][1:18]:
                 continue
+            fields_to_update_after_loading_documents.append([
+                'issues',
+                record['v880'][0]['_'][1:18],
+                'v4',
+                record['v4'][0]['_']
+            ])
 
-            if not document_meta:
-                logger.error(
-                    'Fail to load document into Articlemeta (%s)',
-                    '_'.join([item[0], item[1]])
-                )
-                continue
+    # bulk residual data
+    ctrl.bulk_data(dict(bulk))
 
-            try:
-                rc.add_document(json.dumps(document_meta))
-            except ServerError:
-                logger.error(
-                    'Fail to load document into Articlemeta (%s)',
-                    '_'.join([item[0], item[1]])
-                )
-                continue
+    logger.info('Updating fields metadata')
+    # ctrl.bulk_update_field('issues', fields_to_update_after_loading_documents)
+    logger.info('Loading legacy identifiers')
+    legacy_documents = set(ctrl.articles_ids)
+    legacy_issues = set(ctrl.issues_ids)
+    legacy_journals = set(ctrl.journals_ids)
 
-            logger.debug(
-                'Document (%d, %d) loaded into Articlemeta (%s)',
-                ndx, len(new_documents),
+    logger.info('Producing lists of differences between ArticleMeta and Legacy databases')
+    new_documents = list(legacy_documents - articlemeta_documents)
+    new_issues = list(legacy_issues - articlemeta_issues)
+    new_journals = list(legacy_journals - articlemeta_journals)
+    am_document_pids_only = set([i[0:27] for i in articlemeta_documents])
+    lg_document_pids_only = set([i[0:27] for i in legacy_documents])
+    to_remove_documents = list(am_document_pids_only - lg_document_pids_only)
+
+    am_issue_pids_only = set([i[0:21] for i in articlemeta_issues])
+    lg_issue_pids_only = set([i[0:21] for i in legacy_issues])
+    to_remove_issues = list(am_issue_pids_only - lg_issue_pids_only)
+
+    # Including and Updating Journals
+    logger.info(
+        'Journals being included into articlemeta (%d)',
+        len(new_journals)
+    )
+
+    data = {
+        "journals": [],
+        "issues": []
+    }
+
+    for ndx, item in enumerate(new_journals, 1):
+        item = item.split('_')
+
+        try:
+            journal_meta = ctrl.load_journal(item[0], item[1])
+        except:
+            logger.error(
+                'Fail to load journal into Articlemeta (%s)',
                 '_'.join([item[0], item[1]])
             )
-
-        # Removing Journals
-        total_to_remove_journals = len(to_remove_journals)
-        logger.info(
-            'Journals to be removed from articlemeta (%d)',
-            total_to_remove_journals
-        )
-
-        skip_deletion = True
-        if total_to_remove_journals > SECURE_JOURNAL_DELETIONS_NUMBER:
-            logger.info('To many journals to be removed')
-            if force_delete is False:
-                skip_deletion = True
-                logger.info('force_delete is setup to %s, the remove task will be skipped', force_delete)
-            else:
-                skip_deletion = False
-
-        for ndx, item in enumerate(to_remove_journals, 1):
-            item = item.split('_')
-            if skip_deletion is True:
-                logger.debug(
-                    'Journal remove task (%d, %d) will be skipped (%s)',
-                    ndx,
-                    total_to_remove_journals,
-                    '_'.join([item[0], item[1]])
-                )
-            try:
-                rc.delete_journal(item[1], item[0])
-                logger.debug(
-                    'Journal (%d, %d) removed from Articlemeta (%s)',
-                    ndx,
-                    total_to_remove_journals,
-                    '_'.join([item[0], item[1]])
-                )
-            except UnauthorizedAccess:
-                logger.warning('Unauthorized access to remove itens, check the ArticleMeta admin token')
-
-        # Including and Updating Journals
-        logger.info(
-            'Journals being included into articlemeta (%d)',
-            len(new_journals)
-        )
-        for ndx, item in enumerate(new_journals, 1):
-            item = item.split('_')
-            try:
-                journal_meta = ctrl.load_journal(item[0], item[1])
-            except:
-                logger.error(
-                    'Fail to load journal into Articlemeta (%s)',
-                    '_'.join([item[0], item[1]])
-                )
-                continue
-            if not journal_meta:
-                logger.error(
-                    'Fail to load journal into Articlemeta (%s)',
-                    '_'.join([item[0], item[1]])
-                )
-                continue
-
-            try:
-                rc.add_journal(json.dumps(journal_meta))
-            except ServerError:
-                logger.error(
-                    'Fail to load journal into Articlemeta (%s)',
-                    '_'.join([item[0], item[1]])
-                )
-                continue
-
-            logger.debug(
-                'Journal (%d, %d) loaded into Articlemeta (%s)',
-                ndx,
-                len(new_journals),
+            continue
+        if not journal_meta:
+            logger.error(
+                'Fail to load journal into Articlemeta (%s)',
                 '_'.join([item[0], item[1]])
             )
+            continue
 
-        # Removing Issues
-        total_to_remove_issues = len(to_remove_issues)
-        logger.info(
-            'Issues to be removed from articlemeta (%d)',
-            total_to_remove_issues
-        )
+        data["journals"].append(json.dumps(journal_meta))
 
-        skip_deletion = True
-        if total_to_remove_issues > SECURE_ISSUE_DELETIONS_NUMBER:
-            logger.info('To many issues to be removed')
-            if force_delete is False:
-                skip_deletion = True
-                logger.info('force_delete is setup to %s, the remove task will be skipped', force_delete)
-            else:
-                skip_deletion = False
+    # Including and Updating Issues
+    logger.info(
+        'Issues being included into articlemeta (%d)',
+        len(new_issues)
+    )
+    for ndx, item in enumerate(new_issues, 1):
+        item = item.split('_')
 
-        for ndx, item in enumerate(to_remove_issues, 1):
-            item = item.split('_')
-            if skip_deletion is True:
-                logger.debug(
-                    'Issue remove task (%d, %d) will be skipped (%s)',
-                    ndx,
-                    total_to_remove_issues,
-                    '_'.join([item[0], item[1]])
-                )
-            try:
-                rc.delete_issue(item[1], item[0])
-                logger.debug(
-                    'Issue (%d, %d) removed from Articlemeta (%s)',
-                    ndx,
-                    total_to_remove_issues,
-                    '_'.join([item[0], item[1]])
-                )
-            except UnauthorizedAccess:
-                logger.warning('Unauthorized access to remove itens, check the ArticleMeta admin token')
-
-        # Including and Updating Issues
-        logger.info(
-            'Issues being included into articlemeta (%d)',
-            len(new_issues)
-        )
-        for ndx, item in enumerate(new_issues, 1):
-            item = item.split('_')
-
-            try:
-                issue_meta = ctrl.load_issue(item[0], item[1])
-            except:
-                logger.error(
-                    'Fail to load issue into Articlemeta (%s)',
-                    '_'.join([item[0], item[1]])
-                )
-                continue
-
-            if not issue_meta:
-                logger.error(
-                    'Fail to load issue into Articlemeta (%s)',
-                    '_'.join([item[0], item[1]])
-                )
-                continue
-
-            try:
-                rc.add_issue(json.dumps(issue_meta))
-            except ServerError:
-                logger.error(
-                    'Fail to load issue into Articlemeta (%s)',
-                    '_'.join([item[0], item[1]])
-                )
-                continue
-
-            logger.debug(
-                'Issue (%d, %d) loaded into Articlemeta (%s)',
-                ndx,
-                len(new_issues),
+        try:
+            issue_meta = ctrl.load_issue(item[0], item[1])
+            data["issues"].append(json.dumps(issue_meta))
+        except:
+            logger.error(
+                'Fail to load issue into Articlemeta (%s)',
                 '_'.join([item[0], item[1]])
             )
+            continue
+
+        if not issue_meta:
+            logger.error(
+                'Fail to load issue into Articlemeta (%s)',
+                '_'.join([item[0], item[1]])
+            )
+            continue
+
+        logger.debug(
+            'Issue (%d, %d) loaded into Articlemeta (%s)',
+            ndx,
+            len(new_issues),
+            '_'.join([item[0], item[1]])
+        )
 
     logger.info('Process Isis2mongo Finished')
+    import pdb; pdb.set_trace()
 
-
-def main():
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Dump accesses'
     )
